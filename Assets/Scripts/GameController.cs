d6c343093e82fa30001d7112b2f96fdc717903ec
Wanottah <wanottah@gmail.com>
2023-12-03 12:35:46 +0000

    Original source code

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//
+// Adventure 1980 v2020.11.16
+//
+// created 2020.11.16
+//
+
+
+public class GameController : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+} // end of class
